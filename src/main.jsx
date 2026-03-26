70960573cbe55f7263d4cd2f8b899847e0b10ee7
Andrés Machetá <sr.macheta08@gmail.com>
2026-03-26 15:39:33 -0500

    feat: initial state of calendar app

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { CalendarApp } from "./CalendarApp";
+
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(<CalendarApp />);
