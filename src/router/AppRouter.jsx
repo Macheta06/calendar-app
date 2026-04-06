7e5601b02e4f2b4cb8218408d68f4a35d829ede7
Andrés Machetá <sr.macheta08@gmail.com>
2026-04-06 16:01:36 -0500

    Use state in AppRouter to stay login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,24 @@
 import { Navigate, Route, Routes } from "react-router";
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
-  const authStatus = "not-authenticated";
+  // const authStatus = "not-authenticated";
+  const { status, checkAuthToken } = useAuthStore();
 
+  useEffect(() => {
+    checkAuthToken();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (status === "checking") {
+    return <h3>Cargando...</h3>;
+  }
   return (
     <Routes>
-      {authStatus === "not-authenticated" ? (
+      {status === "not-authenticated" ? (
         <Route path="/auth/*" element={<LoginPage />} />
       ) : (
         <Route path="/*" element={<CalendarPage />} />
